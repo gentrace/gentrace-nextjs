0c82c043e65ef6a0320456d6360eafa1ddc4d5fc
Vivek Nair <vivek@gentrace.ai>
2025-06-26 15:42:15 -0400

    feat: Add OpenTelemetry SDK for Node.js and ConsoleSpanExporter

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,14 +1,22 @@
-import { registerOTel } from "@vercel/otel";
-import { OTLPHttpJsonTraceExporter } from "@vercel/otel";
+import {
+  ConsoleSpanExporter,
+  SimpleSpanProcessor,
+} from "@opentelemetry/sdk-trace-node";
+import { OTLPHttpJsonTraceExporter, registerOTel } from "@vercel/otel";
+
+const traceExporter = new OTLPHttpJsonTraceExporter({
+  url: "https://gentrace.ai/api/otel/v1/traces",
+  headers: {
+    authorization: `Bearer ${process.env.GENTRACE_API_KEY}`,
+  },
+});
 
 export function register() {
   registerOTel({
-    serviceName: "haiku-app",
-    traceExporter: new OTLPHttpJsonTraceExporter({
-      url: "https://gentrace.ai/api/otel/v1/traces",
-      headers: {
-        authorization: `Bearer ${process.env.GENTRACE_API_KEY}`,
-      },
-    }),
+    serviceName: "next-app",
+    spanProcessors: [
+      new SimpleSpanProcessor(traceExporter),
+      new SimpleSpanProcessor(new ConsoleSpanExporter()),
+    ],
   });
 }
